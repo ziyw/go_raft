ca186e8cae52d43a044afaa3d7b40f287b6eeb64
ziyw <zywong159@gmail.com>
2020-09-06 14:54:03 -0400

    Created skeleton for servers

--- main.go
+++ main.go
@@ -2,35 +2,86 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 	"io/ioutil"
+	"log"
+	"net"
 	"os"
 )
+
+type Server struct {
+	Name string
+	Addr string
+
+	// leader state
+	nextIndex  []uint32
+	matchIndex []uint32
+
+	// common server state
+	commitedIndex uint32
+	lastApplied   uint32
+	// persist states
+	currentTerm uint32
+	votedFor    uint32
+	log         []LogEntry
+}
 
 type LogEntry struct {
 	command string
 	term    uint32
 }
 
-type ServerState struct {
-	// persist state, update before respond to rpc
-	currentTerm uint32
-	votedFor    uint32
-	log         []LogEntry
+func (s *Server) Start() {
+	lis, err := net.Listen("tcp", s.Addr)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
 
-	// versatile state
-	commitedIndex uint32
-	lastApplied   uint32
+	_s := grpc.NewServer()
+	RegisterRaftServiceServer(_s, s)
+	if err := _s.Serve(lis); err != nil {
+		log.Fatalf("Failed to server %v", err)
+	}
 }
 
-type LeaderState struct {
-	serverState ServerState
-	// volatile state
-	nextIndex  []uint32
-	matchIndex []uint32
+func (s *Server) AppendEntries(ctx context.Context, arg *AppendArg) (*AppendRes, error) {
+
+	// TODO: insert append logic
+	return &AppendRes{Term: 1, Success: true}, nil
+}
+
+func (s *Server) RequestVote(ctx context.Context, arg *VoteArg) (*VoteRes, error) {
+	return &VoteRes{}, nil
+}
+
+func (s *Server) SendAppendRequest(other Server) {
+	conn, err := grpc.Dial(other.Addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Connection error %v", err)
+	}
+	defer conn.Close()
+
+	client := NewRaftServiceClient(conn)
+	req := AppendArg{}
+	r, err := client.AppendEntries(context.Background(), &req)
+	if err != nil {
+		log.Fatalf("Service Failed %v", err)
+	}
+	log.Println("Reply %s", r.Term)
 }
 
 func main() {
 
+	s1 := Server{Name: "NodeOne", Addr: "localhost:60001"}
+	s2 := Server{Name: "NodeTwo", Addr: "localhost:60002"}
+
+	go s1.Start()
+	go s2.Start()
+
+}
+
+func fileIo() {
 	f, err := os.Create("data.txt")
 	if err != nil {
 		panic(err)
@@ -44,4 +95,5 @@ func main() {
 	}
 
 	fmt.Println("Finish")
+
 }
