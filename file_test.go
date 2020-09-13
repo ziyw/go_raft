7bddc3569e40b0e96438110a728f95269019d0a7
ziyw <zywong159@gmail.com>
2020-09-13 15:15:16 -0400

    read write file done

--- file_test.go
+++ file_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 // unit test for file io
 
@@ -8,6 +11,17 @@ func TestReadSingleValueFromFile(t *testing.T) {}
 
 func TestReadArrayFromFile(t *testing.T) {}
 
-func TestWriteSingleValueToFile(t *testing.T) {}
-
 func TestWriteArrayToFile(t *testing.T) {}
+
+func TestWriteEntryToFile(t *testing.T) {}
+
+func TestReadEntryFromFile(t *testing.T) {}
+
+func TestAppendLine(t *testing.T) {
+	f := "test.txt"
+	l1, l2 := "Line One", "Line Two"
+	AppendLine(f, l1)
+	AppendLine(f, l2)
+	result := ReadLines(f)
+	fmt.Printf("%#v", result)
+}
--- fileio.go
+++ fileio.go
@@ -1,49 +1,68 @@
-// TODO: need to write simple test cases for it.
-
 // Handling read and write from file.
 
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"bufio"
+	_ "fmt"
 	"log"
 	"os"
-	"strconv"
-	"strings"
+	_ "strconv"
+	_ "strings"
 )
 
-func ReadInt(file string) int64 {
-	content, err := ioutil.ReadFile(file)
+func AppendLine(file string, content string) {
+	f, err := os.OpenFile(file, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
-	s := strings.TrimSpace(string(content))
-	v, _ := strconv.ParseInt(s, 10, 64)
-	return v
-}
-
-func WriteInt(file string, content int) {
-	message := []byte(fmt.Sprintf("%d\n", content))
-	err := ioutil.WriteFile(file, message, 0644)
-	if err != nil {
+	if _, err := f.Write([]byte(content)); err != nil {
+		f.Close()
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func fileIo() {
-	f, err := os.Create("data.txt")
+func WriteLine(file string, content string) {
+	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE, 0755)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+	if _, err := f.Write([]byte(content)); err != nil {
+		f.Close()
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ReadLines(file string) []string {
+	f, err := os.Open(file)
+	check(err)
 	defer f.Close()
 
-	data := []byte("HelloWorld\n")
-	err = ioutil.WriteFile("data", data, 0644)
-	if err != nil {
-		panic(err)
+	// limit of lines, not allow big files
+	result := make([]string, 0, 100)
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		result = append(result, string(scanner.Bytes()))
 	}
 
-	fmt.Println("Finish")
-
+	err = scanner.Err()
+	check(err)
+	return result
 }
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// TODO: parse string to Entries
+// TODO: parse string to int  strconv.ParseInt(s, 10, 64)
+// TODO: parse string to antying
